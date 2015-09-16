17af627aaa75b1e90f83954a6c87b3196b66c074
MintPaw <jerusanders@gmail.com>
2015-09-16 15:25:33 -0600

    Got gif output to work

--- src/Main.hx
+++ src/Main.hx
@@ -1,5 +1,7 @@
 package;
 
+import sys.FileSystem;
+
 class Main
 {
 	public static var prog:String = "avconv";
@@ -14,34 +16,34 @@ class Main
 
 		var url:String = input("Youtube link");
 		Sys.println("Downloading...");
-		Sys.command("youtube-dl", ["https://www.youtube.com/watch?v=hICY79NlJNM", "-oraw.mp4" ]);
+		Sys.command("youtube-dl", [url, "-oraw.mp4" ]);
 
 		Sys.println("");
 		Sys.println("Download complete");
 
 		while (true) {
-			var startString:String = input("Where would you like to START the split (hh:mm:ss)");
-			var endString:String = input("Where would you like to END the split (hh:mm:ss)");
-
-			var startSec:Int = Std.parseInt(startString.split(":")[0]) * 3600 + Std.parseInt(startString.split(":")[1]) * 60 + Std.parseInt(startString.split(":")[3]);
-			var endSec:Int = Std.parseInt(endString.split(":")[0]) * 3600 + Std.parseInt(endString.split(":")[1]) * 60 + Std.parseInt(endString.split(":")[3]);
+			var startString:String = input("Where would you like to start the split (hh:mm:ss)");
+			var lengthString:String = input("How long do you want the clip to be? (in seconds)");
 
 			Sys.println("Splitting...");
-			Sys.command("ffmpeg", ["-ss " + Std.string(startSec), "-t " + Std.string(endSec - startSec), "-i raw.mp4" ,"-vcodec copy", "-acodec copy", "split.mp4"]);
+			Sys.command('ffmpeg -i raw.mp4 -ss $startString -t $lengthString split.mp4');
 			Sys.println("Split complete, review this file at split.mp4");
 
 			var ans:String = input("Is this correct? (y/n)");
 
 			if (ans.toLowerCase() == "y") break;
+			FileSystem.deleteFile("split.mp4");
 		}
 
+		Sys.command("mkdir parts");
 		Sys.println("Converting... 00%");
-		Sys.command("mkdir", ["parts"]);
-		Sys.command("ffmpeg", ["-i raw.mp4", "-s 480x270", "-f parts/image2 %03d.png"]);
+		// Sys.command("ffmpeg -i split.mp4 -s 480x270 parts/image-%03d.png");
+		Sys.command("ffmpeg -i split.mp4 -s 480x270 -pix_fmt rgb24 out_small.gif");
+		Sys.command("ffmpeg -i split.mp4 -s 1280x720 -pix_fmt rgb24 out_medium.gif");
 		Sys.println("Converting... 10%");
 
 		// ffmpeg -i video.mkv -s 480x270 -f image2 %03d.png
-		// ffmpeg -ss 14:55 -i video.mkv -t 5 -s 480x270 -f image2 %03d.png
+		// ffmpeg -ss 14:55 -i video.mkv -t 5 -s 480x270 -f image2%05d.png
 	}
 
 	public static function input(prompt:String):String
